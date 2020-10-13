d8b816da5ff300119e42738284c67b5816aa6d96
Việt Hải Lê Đình <haiviet32@gmail.com>
2020-10-13 15:13:51 +0700

    no message

--- src/components/Homepage/TeespringHome.vue
+++ src/components/Homepage/TeespringHome.vue
@@ -1,31 +1,31 @@
 <template>
-<div class="mx-auto container">
-    <div class="grid grid-cols-2 bg-white fixed top-0 container">
-        <div class="inline-flex font-light text-sm py-6">
+<div class="mx-auto">
+    <div class="grid grid-cols-2 bg-white fixed top-0 w-full ">
+        <div class="inline-flex font-light text-sm py-6 ml-24">
             <div class="pr-10"><img style="width: 120px" src="../../assets/image/teespring.png"/></div>
             <div class="pr-10">How it works</div>
             <div class="pr-10">Sell anything</div>
             <div class="pr-10">Sell everywhere</div>
         </div>
-        <div class="flex justify-end py-4">
+        <div class="flex justify-end py-4 mr-24">
             <button class="bg-red-600 text-white font-medium text-sm py-2 px-4 rounded">Start Designing</button>
             <div class="pl-8 pt-2 text-red-600 font-medium">login</div>
         </div>
     </div>
-    <div class="grid grid-cols-2 bg-black mt-20 w-full">
+    <div class="grid grid-cols-2 bg-black mt-20 ">
         <div class="ml-24">
             <h1 class="text-6xl font-black text-white mt-4 ">Commerce. <br>Not Complicated.</h1>
             <p class="text-white pt-2 pb-6 text-sm">Teespring is a free end to end solution for your e-commerce business. Create & <br>sell online.</p>
             <button class="bg-red-600 text-white font-medium text-sm py-2 px-6 rounded-sm mb-56">Get Started</button>
         </div>
-        <div class="bg-cover bg-center w-full" style="background-image: url('image/heroCropGraphic2Web.png')"></div>
+        <div class="bg-cover bg-center" style="background-image: url('image/heroCropGraphic2Web.png')"></div>
     </div>
-    <div class="grid grid-cols-3 gap-6 -mt-32 mb-20">
+    <div class="grid grid-cols-3 gap-6 -mt-32 mb-20 container mx-auto">
         <div class="shadow-2xl"><img class="rounded-sm" src="../../assets/image/tiktok_homepage_v2.png"/></div>
         <div class="shadow-2xl"><img class="rounded-sm" src="../../assets/image/YouTubeTile.jpg"/></div>
         <div class="shadow-2xl"><img class="rounded-sm" src="../../assets/image/CreatorTile.jpg"/></div>
     </div>
-    <div class="grid grid-cols-2 bg-gray-200 ">
+    <div class="grid grid-cols-2 bg-gray-200 container mx-auto ">
         <div class="ml-20 my-6">
             <p class="font-extrabold text-5xl mb-6">Browse products <br>created with<br> Teespring</p>
             <div class="inline-flex">
@@ -38,14 +38,14 @@
             <img class="mt-10 ml-20" style="height: 250px" src="../../../public/image/searchProducts.png"/>
         </div>
     </div>
-    <div class="grid grid-cols-1 bg-gray-900 ">
+    <div class="grid grid-cols-1 bg-gray-900 container mx-auto">
         <div class="flex items-center my-6 ml-48">
         <p class="text-white font-medium">Design your own products?</p>
         <p class="text-white">Teespring makes it easy to create, sell, and order custom products.</p>
         <button class="bg-red-600 text-white font-medium text-sm py-3 px-8 rounded ml-8">Start Designing</button>
         </div>
     </div>
-    <div class="grid grid-cols-1 ">
+    <div class="grid grid-cols-1 container mx-auto">
         <div class="my-12"><h2 class="font-extrabold text-2xl">Recommendations For You</h2></div>
         <div class="inline-flex">
             <div class="pr-8"><img style="width:309px; height: 350px" src="../../assets/image/1.jpg"/>
@@ -74,7 +74,7 @@
             </div>
         </div>
     </div>
-    <div class="grid grid-cols-1 ">
+    <div class="grid grid-cols-1 container mx-auto">
         <div class="my-12"><h2 class="font-extrabold text-2xl">Trending</h2></div>
         <div class="inline-flex">
             <div class="pr-8"><img class="bg-gray-600" style="width:309px; height: 350px" src="../../assets/image/1.jpg"/>
@@ -103,7 +103,7 @@
             </div>
         </div>
     </div>
-    <div class="grid grid-cols-1 mt-6 mb-20">
+    <div class="grid grid-cols-1 mt-6 mb-20 container mx-auto">
         <div class="text-2xl font-extrabold my-6">Featured</div>
         <div class="inline-flex">
             <div class=""><img src="../../assets/image/5.png"/></div>
@@ -112,7 +112,7 @@
         </div>
     </div>
     <div class="border-t"></div>
-    <div class="grid grid-cols-5 mt-12">
+    <div class="grid grid-cols-5 mt-12 container mx-auto">
         <div class="">
             <div class="mb-10" style="width: 42px;height: 18px"><svg>
                 <path d="M27.22 2.52c-1.328 0-2.408 1.06-2.408 2.363 0 1.855.7 3.55 1.854 4.852.478.538 1.033 1.008 1.65 1.393.37-.553.68-1.15.918-1.78.358-.95.554-1.977.554-3.048 0-.636-.07-1.256-.202-1.855-.01-.05-.023-.1-.035-.15-.267-1.02-1.21-1.775-2.33-1.775zM12.364 9.348c.238.63.548 1.227.92 1.78.615-.385 1.17-.855 1.648-1.393 1.155-1.3 1.856-2.997 1.856-4.852 0-1.303-1.08-2.363-2.41-2.363-1.12 0-2.062.756-2.33 1.774-.01.05-.024.1-.035.15-.13.6-.2 1.22-.2 1.856 0 1.07.195 2.098.553 3.048zm26.74-1.156L41.4 9.32c-1.66 3.25-5.085 5.484-9.044 5.484-1.164 0-2.28-.195-3.322-.55-2.097 2.088-5.01 3.385-8.235 3.385-3.225 0-6.14-1.298-8.236-3.387-1.04.356-2.16.55-3.323.55-4.12 0-7.665-2.42-9.24-5.89l2.35-1.025c1.176 2.587 3.82 4.396 6.89 4.396.56 0 1.107-.062 1.632-.176-1.035-1.7-1.63-3.685-1.63-5.808 0-.808.087-1.597.25-2.357.024-.11.05-.216.077-.324C10.136 1.534 12.073 0 14.376 0c.046 0 .09.002.136.003.14.004.28.013.418.028 2.49.27 4.424 2.34 4.424 4.853 0 3.378-1.722 6.36-4.35 8.15 1.566 1.3 3.588 2.086 5.795 2.086 2.206 0 4.228-.787 5.794-2.086-2.628-1.79-4.35-4.773-4.35-8.15C22.243 2.37 24.177.3 26.665.03c.138-.014.277-.023.418-.027.046 0 .09-.003.137-.003 2.304 0 4.24 1.535 4.808 3.62.027.107.053.214.077.323.164.76.25 1.55.25 2.357 0 2.123-.595 4.11-1.63 5.808.526.114 1.07.176 1.63.176 2.95 0 5.506-1.668 6.747-4.092z"></path>
@@ -198,7 +198,7 @@
             </ul>
         </div>
     </div>
-    <div class="grid grid-cols-1">
+    <div class="grid grid-cols-1 container mx-auto">
         <div class="flex text-gray-600 text-sm font-light my-10">
         <p class="">© 2020 Teespring, Inc.</p>
         <p class="mx-2">Terms of service</p>
