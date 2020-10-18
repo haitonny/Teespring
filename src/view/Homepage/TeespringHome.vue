b0ba0681d6649b8984b3cb9e3b3a6f5e77151be5
Việt Hải Lê Đình <haiviet32@gmail.com>
2020-10-18 17:39:50 +0700

    no message

--- src/view/Homepage/TeespringHome.vue
+++ src/view/Homepage/TeespringHome.vue
@@ -2,35 +2,34 @@
   <div class="mx-auto">
     <div class="bg-white fixed top-0 w-full flex justify-between">
       <div class="font-light text-sm py-6 flex lg:ml-16 xl:ml-24">
-        <div class="pr-10 pr-4"><img style="width: 120px" src="../../assets/image/teespring.png" /></div>
-        <div class="pr-10 pr-4">How it works</div>
-        <div class="pr-10 pr-4">Sell anything</div>
-        <div class="pr-10 pr-4">Sell everywhere</div>
+        <div class="ml-4 lg:ml-0"><img style="width: 120px" src="../../assets/image/teespring.png" /></div>
+        <div class="hidden lg:flex ml-10">
+          <div class="pr-10 pr-4">How it works</div>
+          <div class="pr-10 pr-4">Sell anything</div>
+          <div class="pr-10 pr-4">Sell everywhere</div>
+        </div>
       </div>
-      <div class="py-4 mr-24 flex">
+      <div class="py-4 lg:mr-16 xl:mr-24 hidden lg:flex">
         <button class="bg-red-600 text-white font-medium text-sm py-2 px-4 rounded">Start Designing</button>
         <div class="pl-8 pt-2 text-red-600 font-semibold hover:text-red-700 cursor-pointer">Login</div>
       </div>
     </div>
-    <div class="grid grid-cols-2 bg-current">
-      <div class="lg:ml-20 xl:ml-32 lg:mt-32 xl:mt-10 lg:mb-20 xl:mb-0 text-left ">
-        <h1 class="lg:text-5xl xl:text-6xl font-black text-white mt-12">Commerce. <br />Not Complicated.</h1>
+    <div class="grid grid-cols-1 lg:grid-cols-2 bg-current bg-gradient-to-r from-blue-900">
+      <div class="lg:ml-20 xl:ml-32 mt-32 lg:mt-32 xl:mt-10 lg:mb-20 xl:mb-0 text-left text-center lg:text-left">
+        <h1 class="text-4xl lg:text-5xl xl:text-6xl font-black text-white mt-12">Commerce. <br />Not Complicated.</h1>
         <p class="text-white pt-2 pb-6 text-sm">Teespring is a free end to end solution for your e-commerce business. Create & <br />sell online.</p>
-        <button class="bg-red-600 text-white font-medium text-sm py-2 px-6 rounded-sm mb-56">Get Started</button>
+        <button class="bg-red-600 text-white font-medium text-sm py-2 px-6 rounded-sm mb-0 lg:mb-56">Get Started</button>
       </div>
       <div class="bg-contain bg-center bg-left-top bg-no-repeat mt-20 xl:mt-20" style="background-image: url('image/heroCropGraphic2Web.png')"></div>
     </div>
-    <div class="grid grid-cols-3 gap-6 -mt-32 mb-20 lg:mx-20 xl:container mx-auto">
+    <div class="grid grid-cols-1 lg:grid-cols-3 gap-6 mt-0 mx-4 lg:-mt-32 mb-20 lg:mx-20 xl:container xl:mx-auto">
       <div class="shadow-2xl"><img class="rounded-sm" src="../../assets/image/tiktok_homepage_v2.png" /></div>
       <div class="shadow-2xl"><img class="rounded-sm" src="../../assets/image/YouTubeTile.jpg" /></div>
       <div class="shadow-2xl"><img class="rounded-sm" src="../../assets/image/CreatorTile.jpg" /></div>
     </div>
-    <div class="bg-gray-200 lg:mx-12 xl:mx-auto xl:container flex">
+    <div class="bg-gray-200 lg:mx-12 xl:mx-auto xl:container hidden lg:flex">
       <div class="ml-20 my-6">
-        <p class="font-extrabold text-5xl mb-6">
-          Browse products <br />created with<br />
-          Teespring
-        </p>
+        <p class="font-extrabold text-5xl mb-6">Browse products <br />created with<br />Teespring</p>
         <div class="inline-flex">
           <div class="pt-4 px-3 bg-white">
             <img class="" style="width: 15px; height: 15px" src="../../assets/image/search-magnifier-outline-svgrepo-com.svg" alt="" />
@@ -43,7 +42,25 @@
         <img class="mt-10 lg:ml-4 xl:ml-64" style="height: 250px" src="../../../public/image/searchProducts.png" />
       </div>
     </div>
-    <div class="grid grid-cols-1 bg-gray-900 lg:mx-12 xl:container mx-auto">
+    <div class="bg-contain bg-center bg-no-repeat bg-gray-500 lg:hidden" style="background-image: url('image/searchProducts.png');">
+      <div class="text-center py-40">
+        <p class="text-4xl font-bold text-white leading-tight mb-8">
+          Browse products<br />
+          created with Teespring
+        </p>
+        <div class="inline-flex">
+          <div class="pt-4 px-3 bg-white">
+            <img class="" style="width: 15px; height: 15px" src="../../assets/image/search-magnifier-outline-svgrepo-com.svg" alt="" />
+          </div>
+          <input type="text" placeholder="search the marketplace " class="py-3 outline-none w-56" />
+          <button class="bg-red-600 text-white font-medium text-sm py-3 px-8">Go</button>
+        </div>
+      </div>
+      <div class="bg-blue-900 text-white text-sm font-normal bg-opacity-75 text-center">
+        <div class="py-3"><a href="">Shop by Categories</a></div>
+      </div>
+    </div>
+    <div class="grid grid-cols-1 bg-gray-900 lg:mx-12 xl:container mx-auto" v-if="!responsive.isMobile">
       <div class="flex items-center my-6 lg:ml-4 xl:ml-48">
         <p class="text-white font-medium">Design your own products?</p>
         <p class="text-white">Teespring makes it easy to create, sell, and order custom products.</p>
@@ -118,7 +135,7 @@
         </div>
       </div>
     </div>
-    <div class="grid grid-cols-1 mt-6 mb-20 lg:mx-12 xl:container mx-auto">
+    <div class="grid hidden lg:block grid-cols-1 mt-6 mb-20 lg:mx-12 xl:container mx-auto">
       <div class="text-2xl font-extrabold my-6">Featured</div>
       <div class="inline-flex">
         <div class=""><img src="../../assets/image/5.png" /></div>
@@ -126,12 +143,28 @@
         <div class=""><img src="../../assets/image/7.png" /></div>
       </div>
     </div>
+    <div class="mx-4 lg:hidden">
+      <h1 class="text-center font-bold text-3xl my-3">Featured Collections</h1>
+      <div class="grid grid-cols-1 mb-8">
+        <div class=""><img src="../../assets/image/5.png" height="420" width="740" /></div>
+      </div>
+      <div class="grid grid-cols-2 gap-8">
+        <div class=""><img src="../../assets/image/6.png" height="420" width="600" /></div>
+        <div class=""><img src="../../assets/image/7.png" height="420" width="600" /></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TeespringHome"
+  name: "TeespringHome",
+  inject: ["responsive"],
+  computed: {
+    responsiveValue() {
+      return this.responsive.isMobile ? "Mobile" : "Desktop";
+    }
+  }
 };
 </script>
 
