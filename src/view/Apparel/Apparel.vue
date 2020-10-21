56864ad5d2642bcb87fe42389a170ccf426d602c
Việt Hải Lê Đình <haiviet32@gmail.com>
2020-10-21 15:54:39 +0700

    no message

--- src/view/Apparel/Apparel.vue
+++ src/view/Apparel/Apparel.vue
@@ -3,15 +3,15 @@
     <header-shop />
     <div class="bg-gradient-to-r from-purple-900  via-blue-600 to-pink-600">
       <div class="grid grid-cols-2 mx-auto container">
-        <div class="ml-4 lg:ml-20 my-10 text-white">
+        <div class="sm:ml-4 ml-4 xl:ml-20 my-10 text-white">
           <h1 class="font-bold text-4xl">Apparel</h1>
-          <p class="font-medium hidden xl:block">Shop for Apparel shirts, hoodies and gifts. Find Apparel designs printed with care on top quality garments.</p>
+          <p class="font-medium hidden lg:block">Shop for Apparel shirts, hoodies and gifts. Find Apparel designs printed with care on top quality garments.</p>
         </div>
         <div class=""></div>
       </div>
     </div>
-    <div class="grid grid-cols-3 lg:grid-cols-4 gap-4 mt-16 mx-auto container">
-      <div class="hidden lg:block lg:ml-10 xl:ml-20">
+    <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 mt-16 xl:mx-auto container">
+      <div class="hidden lg:block lg:ml-0 xl:ml-20">
         <div class="bg-gray-200 pt-5 pl-4 text-red-700 font-semibold text-sm">
           <a href="" class="block">All Categories</a>
           <a href="" class="block py-6">Apparel</a>
@@ -19,6 +19,16 @@
         <div class="bg-gray-200 pl-4 text-sm font-medium">
           Apparel
           <div class="text-gray-600 pl-6 pb-4">
+            <a href="" class="block my-2">Hobbies</a>
+            <a href="" class="block">Apparel</a>
+            <a href="" class="block my-2">Patriotic</a>
+            <a href="" class="block">Teespring Exclusives</a>
+            <a href="" class="block my-2">Names</a>
+            <a href="" class="block my-2">Hobbies</a>
+            <a href="" class="block">Apparel</a>
+            <a href="" class="block my-2">Patriotic</a>
+            <a href="" class="block">Teespring Exclusives</a>
+            <a href="" class="block my-2">Names</a>
             <a href="" class="block my-2">Hobbies</a>
             <a href="" class="block">Apparel</a>
             <a href="" class="block my-2">Patriotic</a>
@@ -34,10 +44,20 @@
             <a href="" class="block my-2">Patriotic</a>
             <a href="" class="block">Teespring Exclusives</a>
             <a href="" class="block my-2">Names</a>
+            <a href="" class="block my-2">Hobbies</a>
+            <a href="" class="block">Apparel</a>
+            <a href="" class="block my-2">Patriotic</a>
+            <a href="" class="block">Teespring Exclusives</a>
+            <a href="" class="block my-2">Names</a>
+            <a href="" class="block my-2">Hobbies</a>
+            <a href="" class="block">Apparel</a>
+            <a href="" class="block my-2">Patriotic</a>
+            <a href="" class="block">Teespring Exclusives</a>
+            <a href="" class="block my-2">Names</a>
           </div>
         </div>
       </div>
-      <div class="text-center text-sm font-semibold mx-auto pt-4">
+      <div class="text-center text-sm font-semibold mx-auto pt-4 hidden md:block">
         <div class="">
           <img class="" src="../../assets/image/1.jpg" height="340" width="250" />
           <p class="mt-4">Limited Edition.</p>
@@ -51,7 +71,7 @@
           <p class="mt-4">Win some,Lose some,Dim sum</p>
         </div>
       </div>
-      <div class="text-center text-sm font-semibold mx-auto pt-4">
+      <div class="text-center text-sm font-semibold mx-auto pt-4 hidden md:block">
         <div class="">
           <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
           <p class="mt-4">Pizza Lover's Food Pyramid</p>
@@ -65,7 +85,7 @@
           <p class="mt-4">Silliest</p>
         </div>
       </div>
-      <div class="text-center text-sm font-semibold mx-auto pt-4">
+      <div class="text-center text-sm font-semibold mx-auto pt-4 hidden md:block">
         <div class="">
           <img class="" src="../../assets/image/1.jpg" height="340" width="250" />
           <p class="mt-4">Ameowica the Great - Funny patriotic</p>
@@ -80,23 +100,25 @@
         </div>
       </div>
     </div>
-    <div class="grid grid-cols-3 lg:grid-cols-4 ml-20 mt-10">
+    <div class="md:grid hidden md:grid-cols-3 lg:grid-cols-4 ml-0 xl:ml-20 mt-10">
       <div class="hidden lg:block"></div>
       <div class="col-span-3">
         <p class="font-light text-sm mb-4">Related Categories:</p>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold">Patriotic</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mx-4">Artistic</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold">LGBT</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mx-4">For a Cause</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold">Hoodies</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mx-4">Adventure</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold">Names</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mx-4">Trending Collection</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold">Funny</a>
-        <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mx-4">Leggings</a>
+        <div class="flex flex-wrap">
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4">Patriotic</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mx-4 mb-4">Artistic</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4">LGBT</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4 mx-4">For a Cause</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4">Hoodies</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4 mx-4">Adventure</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4">Names</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4 mx-4">Trending Collection</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4">Funny</a>
+          <a href="" class="text-white bg-indigo-900 border text-xs p-2 font-semibold mb-4 ml-4">Leggings</a>
+        </div>
       </div>
     </div>
-    <div class="grid grid-cols-3 lg:grid-cols-4 gap-4 mt-20">
+    <div class="md:grid hidden md:grid-cols-3 lg:grid-cols-4 gap-4 mt-20">
       <div class="hidden lg:block"></div>
       <div class="text-center mx-auto">
         <img src="../../assets/image/1.jpg" height="340" width="250" />
@@ -111,6 +133,100 @@
         <p>Salt Bae Merica Edition</p>
       </div>
     </div>
+    <div class="mx-4 md:hidden">
+      <div class="grid grid-cols-2 mb-20 text-sm gap-8">
+        <div class="">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 text-center">Limited Edition</p>
+        </div>
+        <div class="">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 text-center truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 mb-10 text-sm gap-8">
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Ameowica the Great- Funny</p>
+        </div>
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4">Just Eat it Bugger Lover</p>
+        </div>
+      </div>
+      <div class="">
+        <p class="font-light text-sm mb-4 text-center">Related Categories:</p>
+        <div class="flex ml-0 sm:ml-32">
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold">Funny</a>
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold mx-5">Pocket Tees</a>
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold">Animal Lover</a>
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold ml-5">Beer</a>
+        </div>
+        <div class="my-4 ml-0 sm:ml-32">
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold ml-4">Patriotic</a>
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold mx-6">Trending Collection</a>
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold">Names</a>
+        </div>
+        <div class="ml-0 sm:ml-32">
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold ml-10">Adventure</a>
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold mx-6">Leggings</a>
+          <a href="" class="text-white bg-indigo-900 border rounded text-xs p-2 font-semibold">Hoodies</a>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 my-20 text-sm gap-8">
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 text-sm gap-8">
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate">Pizza Lover's Food Pyramid</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 my-20 text-sm gap-8">
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 text-sm gap-8">
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+        <div class="text-center">
+          <img class="" src="../../assets/image/2.jpg" height="340" width="250" />
+          <p class="mt-4 truncate ">Pizza Lover's Food Pyramid</p>
+        </div>
+      </div>
+    </div>
+    <div class="grid grid-cols-3 lg:grid-cols-4 my-16">
+      <div class=""></div>
+      <div class="col-span-3 m-auto text-sm text-gray-400 flex">
+        <a href="" class="mx-4 font-medium">1</a>
+        <a href="" class="mx-4 font-medium">2</a>
+        <a href="" class="mx-4 font-medium">3</a>
+        <a href="" class="mx-4 font-medium">4</a>
+        <a href="" class="mx-4 font-medium">5</a>
+        <a href="" class="mx-4 font-medium transform rotate-180 " style="margin-bottom: 5px"
+          ><img class="text-gray-300" src="@/assets/image/back.svg" width="12" alt=""
+        /></a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -118,7 +234,13 @@
 import HeaderShop from "@/components/HeaderShop";
 export default {
   name: "Apparel",
-  components: { HeaderShop }
+  components: { HeaderShop },
+  inject: ["responsive"],
+  computed: {
+    responsiveValue() {
+      return this.responsive.isMobile ? "Mobile" : "Desktop";
+    }
+  }
 };
 </script>
 
